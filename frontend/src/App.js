86c0550d74eb694e66545499a0b9ba71914da6ab
Iamdachi04 <dachi4github@gmail.com>
2025-07-23 19:20:21 +0400

    this is what I have yet , but it simply is not working yet ... does not work normally yet ...  I will find the underlying idea , of why it might it not work.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Carousel } from 'react-bootstrap';
+import FeatureCard from './components/FeatureCard'; // Importing the FeatureCard component
 
 function App() {
    const cardData = [
@@ -20,21 +22,24 @@ function App() {
     },
   ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Container className="my-5">
+        <h2 className='mb-5'>the famous singers (the once I love the most!!!!)</h2>
+        {/*ანუ აქ ვწერთ წინასწარ გაწერილ ციესესის სტილებს , როგორიცაა დისფლეი ფლექსი , და justify-content:center*/}
+        <Carousel indicators={true} interval={null} controls={true}>
+          { cardData.map((card_itself , index)=>(
+            <Carousel.Item key={index} className='d-flex justify-content-center' >
+              <FeatureCard 
+              imageSrc={card_itself.imageSrc}
+              title={card_itself.title}
+              description={card_itself.description}
+              buttonText={card_itself.buttonText}
+              buttonVariant={card_itself.buttonVariant}
+              />
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </Container>
     </div>
   );
 }
