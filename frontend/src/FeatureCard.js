3a8d4c27c9f7eed1f4701ead400625dbb97708d2
Iamdachi04 <dachi4github@gmail.com>
2025-07-25 17:40:13 +0400

    created the final normal carousel card , that can be use by App.js

---
+++ frontend/src/FeatureCard.js
@@ -0,0 +1,15 @@
+import { Carousel } from 'react-bootstrap';
+function FeatureCard(props) {
+  return (<>
+      <img className="d-block w-100" src={props.imageSrc} alt={props.title}
+        style={{ height: '500px', objectFit: 'cover' }}
+      />
+      <Carousel.Caption>
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+      </Carousel.Caption>
+    </>
+  );
+}
+
+export default FeatureCard;
