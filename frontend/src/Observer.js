0d7e2bd71a40fd7298e0e43ede257d8444ddc4b3
Iamdachi04 <dachi4github@gmail.com>
2025-07-27 22:04:09 +0400

    created observer , that observes , it makes a hook everytime , usestate is simply To create a piece of memory for the component. useeffect to interact with the browser , useref to get the HTML element in DOM. children is the same as carousel divs.when it renders firstly , isVisible is false , and nothing renders , but then it becomes true , and then it shows the children(the carousel , and the header that I am going to add)

---
+++ frontend/src/Observer.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function Observer({ children }) {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.unobserve(entry.target);
+        }
+      },
+      {
+        threshold: 0.4,
+      }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+
+  return (
+    <div ref={ref} className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}>
+      {children}
+    </div>
+  );
+}
+
+export default Observer;
